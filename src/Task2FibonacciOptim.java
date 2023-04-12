19241bfde6c94a46f575d09f17aacd8e22aceff4
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-12 09:57:24 +0200

    Change fibonacci()

--- src/Task2FibonacciOptim.java
+++ src/Task2FibonacciOptim.java
@@ -5,10 +5,10 @@ public class Task2FibonacciOptim {
 // Оптимизируйте алгоритм для вычисления чисел Фибоначчи (см. этот урок, задача 2),
 // оцените временну́ю и пространственную эффективность.
   public static void main(String[] args) {
-    ArrayList<Integer> numbers=new ArrayList<>();
+    ArrayList<Integer> numbers = new ArrayList<>();
     numbers.add(1);
     numbers.add(1);
-    System.out.println("fibonacci(10) = " + fibonacci(10));
+    System.out.println("fibonacci(10) = " + fibonacci(10, numbers));
     System.out.println(numbers);
   }
 
@@ -18,10 +18,13 @@ public class Task2FibonacciOptim {
    * @param n порядковый номер числа Фибоначчи
    * @return число Фибоначчи
    */
-  public static int fibonacci(int n) {
+  public static int fibonacci(int n, ArrayList<Integer> numbers) {
     if (n < 3) { // для 1 и 2
-      return 1;
+      return numbers.get(n - 1);
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if (numbers.size() < n) {
+      numbers.add(fibonacci(n - 1, numbers) + fibonacci(n - 2, numbers));
+    }
+    return numbers.get(n - 1);
   }
 }
